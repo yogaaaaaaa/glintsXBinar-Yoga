087403e870dcbc2a997c844761e2f2318a0d2113
yogaaaaaaa <yogaadhi@icloud.com>
2021-04-08 09:43:02 +0700

    adding validator auth

--- Week 6/Day 27/middlewares/validators/authValidator.js
+++ Week 6/Day 27/middlewares/validators/authValidator.js
@@ -9,19 +9,23 @@ exports.signup = async (req, res, next) => {
   }
 
   if (!validator.isStrongPassword(req.body.password)) {
-    errors.push("password must contain min 8 chars, min 1 UpperCase, min 1 LowerCase, 1 numb, 1 symbol");
+    errors.push(
+      "password must contain min 8 chars, min 1 UpperCase, min 1 LowerCase, 1 numb, 1 symbol"
+    );
   }
 
-//check pasword confirmation
-if(req.body.confirmPassword !== req.body.password){
+  //check pasword confirmation
+  if (req.body.confirmPassword !== req.body.password) {
     errors.push("password confirmation must be same to password");
-}
-
-
-  if(errors.length > 0){
-      return res.status(400).json({
-          message: errors.join(", "),
-      });
   }
 
+
+  //if error >0, it will maKe errors message joined with ,
+  if (errors.length > 0) {
+    return res.status(400).json({
+      message: errors.join(", "),
+    });
+  }
+
+  next();
 };
