fa6159a4a280decf08f0978a7b143a56bc002052
yogaaaaaaa <yogaadhi@icloud.com>
2021-04-12 12:47:13 +0700

    fixing test

--- Week 6/Day 30/test/test.js
+++ Week 6/Day 30/test/test.js
@@ -10,7 +10,7 @@ const deleteAllData = async () => {
 
 deleteAllData();
 
-describe("User Test", () => {
+describe("Auth Test", () => {
   describe("/auth/signup POST", () => {
     it("it should make user and get the token", async () => {
       const res = await request(app).post("/auth/signup").send({
@@ -29,6 +29,7 @@ describe("User Test", () => {
   });
 });
 
+//test the error
 describe('/auth/signup POST', () => {
     it('It should error while making user', async () => {
       const res = await request(app).post("/auth/signup").send({
@@ -49,13 +50,13 @@ describe('/auth/signup POST', () => {
   describe("/POST Sign In", () => {
     it("It should make user login and get authentication_key (jwt)", async () => {
       const res = await request(app).post("/auth/signin").send({
-        email: "yogadhi@icloud.com",
+        email: "yogaadhi@icloud.com",
         password: "Aneh1234!!",
       });
 
       expect(res.statusCode).toEqual(200);
       expect(res.body).toBeInstanceOf(Object);
-      expect(res.body.message).toEqual("Success");
+      expect(res.body.message).toEqual("sukses");
       expect(res.body).toHaveProperty("token");
 
       authenticationToken = res.body.token;
@@ -64,23 +65,23 @@ describe('/auth/signup POST', () => {
   });
 
 
-// describe("Transaksi Test", () => {
-//   /*
-//    * Test the first /GET route
-//    * There are no data
-//    */
-//   describe("/GET transaksi", () => {
-//     it("it should GET all the transaksi", async () => {
-//       const res = await request(app)
-//         .get("/transaksi")
-//         .set({
-//           Authorization: `Bearer ${authenticationToken}`,
-//         });
+describe("Transaksi Test", () => {
+  /*
+   * Test the first /GET route
+   * There are no data
+   */
+  describe("/GET transaksi", () => {
+    it("it should GET all the transaksi", async () => {
+      const res = await request(app)
+        .get("/transaksi")
+        .set({
+          Authorization: `Bearer ${authenticationToken}`,
+        });
 
-//       expect(res.statusCode).toEqual(404);
-//       expect(res.body).toBeInstanceOf(Object);
-//       expect(res.body).toHaveProperty("message");
-//       expect(res.body.message).toEqual("Transaksi Not Found");
-//     });
-//   });
-// });
+      expect(res.statusCode).toEqual(404);
+      expect(res.body).toBeInstanceOf(Object);
+      expect(res.body).toHaveProperty("message");
+      expect(res.body.message).toEqual("transaksi not found");
+    });
+  });
+});
